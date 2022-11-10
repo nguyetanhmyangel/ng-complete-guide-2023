65339cdc01bff52ee0aaba042faf0eb097244f8c
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-11-10 16:32:48 +0700

    3th commit

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-project';
-  featureSelected!: string;
+  featureSelected: string = 'recipes';
 
   public onNavigate(event: string) {
     console.log('event', event);
--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,11 +9,18 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A another Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
